ded9a70be255d189f7cbaf8e2139e933f0e092b5
Michael Klatt <mdklatt@alumni.ou.edu>
2018-05-03 21:40:32 -0500

    added version file

---
+++ src/version.hpp
@@ -0,0 +1,28 @@
+/// Current version of the pypp library.
+///
+/// This package uses the Semantic Versioning scheme:
+///
+///   <http://semver.org/>
+///
+/// Major versions introduce significant changes to the API, and backwards
+/// compatibility is not guaranteed. Minor versions are for new features and
+/// other backwards-compatible changes to the API. Patch versions are for bug
+/// fixes and internal code changes that do not affect the API. If a devel
+/// version number is defined this is an incomplete development version of a
+/// patch release.
+///
+/// Version 0.x should be considered a development version with an unstable
+/// API, and backwards compatibility is not guaranteed for minor releases.
+///
+/// @file
+#ifndef PYPP_VERSION_HPP
+#define PYPP_VERSION_HPP
+
+
+#define PYPP_MAJOR_VERSION "0"
+#define PYPP_MINOR_VERSION "1"
+#define PYPP_PATCH_VERSION "0"
+#define PYPP_DEVEL_VERSION "1"
+
+
+#endif  // PYPP_VERSION_HPP
