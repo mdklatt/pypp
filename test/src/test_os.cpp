fbf54f577c4765b61c259a221673727013c54410
Michael Klatt <mdklatt@alumni.ou.edu>
2018-11-07 23:34:50 -0600

    Refactored test_os.cpp to use tempfile::TemporaryDirectory

--- test/src/test_os.cpp
+++ test/src/test_os.cpp
@@ -5,7 +5,6 @@
 ///
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
 #include "sys/stat.h"  // TODO: not portable
-#include "unistd.h"  // TODO: not portable
 #else
 #error "os module test suite requires *nix"
 #endif
@@ -23,6 +22,7 @@
 #include "pypp/func.hpp"
 #include "pypp/os.hpp"
 #include "pypp/path.hpp"
+#include "pypp/tempfile.hpp"
 
 
 using pypp::func::in;
@@ -32,67 +32,16 @@ using pypp::path::isdir;
 using pypp::path::join;
 using pypp::path::normpath;
 using pypp::path::split;
+using pypp::tempfile::TemporaryDirectory;
 using std::begin;
 using std::end;
-using std::find;
-using std::getenv;
-using std::remove;
 using std::runtime_error;
 using std::ofstream;
 using std::sort;
 using std::string;
-using std::system;
 using std::vector;
 
 using namespace pypp::os;
-
-
-/// Create a temporary test directory.
-///
-class TemporaryDirectory
-{
-public:
-    string name;
-
-    TemporaryDirectory()
-    {
-        string root("/tmp");
-        for (const auto& var: {"TMPDIR", "TEMP", "TMP"}) {
-            // Attempt to get the system tmp directory from the environment.
-            const auto env(getenv(var));
-            if (env) {
-                root = env;
-                break;
-            }
-        }
-        if (not isdir(root)) {
-            root = ".";  // default to current directory as last resort
-        }
-        name = join({root, "tmpXXXXXXXX"});
-        mkdtemp(&name[0]);  // name is modified in place
-        assert(not name.empty());
-    }
-
-    ~TemporaryDirectory()
-    {
-        // FIXME: This is extremely dangerous.
-        const vector<string> prohibited{
-            // This is certainly not exhaustive. And it relies on other modules
-            // in this library that may not have passed their own test suites
-            // yet, so it might might be altogether useless. At best it's
-            // (probably) better than nothing.
-            "/",
-            abspath("."),
-        };
-        name = abspath(normpath(name));
-        if (in(name, prohibited)) {
-            // Don't just assert() here because this check always needs to
-            // happen.
-            throw runtime_error("refusing to delete " + name);
-        }
-        system(string("rm -rf " + name).c_str());
-    }
-};
 
 
 /// Test the listdir() function.
@@ -135,7 +84,7 @@ TEST(os, removedirs)
     for (const auto& dir: subdirs) {
         // Create subdirectories to remove.
         path = join({path, dir});
-        assert(mkdir(path.c_str(), 0777) == 0);
+        assert(mkdir(path.c_str(), 0700) == 0);
     }
     removedirs(path);  // will also remove tmpdir itself
     const auto pair(split(tmpdir.name));
