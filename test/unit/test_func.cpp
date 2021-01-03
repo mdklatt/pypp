6723296937d56c0969d2b44351ed2f8b1e71b217
Michael Klatt <mdklatt@alumni.ou.edu>
2021-01-03 01:03:25 -0600

    Modified range() functions to use generators

--- test/unit/test_func.cpp
+++ test/unit/test_func.cpp
@@ -3,6 +3,7 @@
 /// Link all test files with the `gtest_main` library to create a command line
 /// test runner.
 ///
+#include <iterator>
 #include <string>
 #include <gtest/gtest.h>
 #include "pypp/pypp.hpp"
@@ -10,6 +11,8 @@
 
 using namespace pypp::func;
 
+using std::begin;
+using std::end;
 using std::pair;
 using std::string;
 using std::vector;
@@ -104,43 +107,82 @@ TEST(func, zip)
 /// that uses this fixture.
 ///
 template <typename T>
-class IncrementalRangeTest: public Test {};
+class IntegralRangeTest: public Test {};
+using IntegralTypes = Types<char, int, long, size_t, ssize_t>;
+TYPED_TEST_CASE(IntegralRangeTest, IntegralTypes);
 
-using IncrementalTypes = Types<char, int, long, size_t, ssize_t>;
-TYPED_TEST_CASE(IncrementalRangeTest, IncrementalTypes);
 
-
-/// Test the incremental range() function.
-///
-TYPED_TEST(IncrementalRangeTest, range)
-{
-    ASSERT_EQ(range(TypeParam{}), vector<TypeParam>({}));
-    ASSERT_EQ(range<TypeParam>(2), vector<TypeParam>({0, 1}));
+/**
+ * Test the integral range() function.
+ */
+TYPED_TEST(IntegralRangeTest, range) {
+    auto values(range(TypeParam(2)));
+    ASSERT_EQ(vector<TypeParam>(begin(values), end(values)), vector<TypeParam>({0, 1}));
 }
 
 
-/// Test fixture for the arithmetic range() function.
-///
-/// This is used to group tests and provide common set-up and tear-down code.
-/// A new test fixture is created for each test to prevent any side effects
-/// between tests. Member variables and methods are injected into each test
-/// that uses this fixture.
-///
+/**
+ * Test the integral range() function for an empty range.
+ */
+TYPED_TEST(IntegralRangeTest, range_empty) {
+    auto values(range(TypeParam(0)));
+    ASSERT_EQ(vector<TypeParam>(begin(values), end(values)), vector<TypeParam>());
+}
+
+
+/**
+ * Test fixture for the arithmetic range() function.
+ *
+ * This is used to group tests and provide common set-up and tear-down code. A
+ * new test fixture is created for each test to prevent any side effects
+ * between tests. Member variables and methods are injected into each test that
+ * uses this fixture.
+ */
 template <typename T>
 class ArithmeticRangeTest: public Test {};
-
 using ArithmeticTypes = Types<int, long, float, double>;  // no unsigned types
 TYPED_TEST_CASE(ArithmeticRangeTest, ArithmeticTypes);
 
 
-/// Test the numeric range() function.
-///
-TYPED_TEST(ArithmeticRangeTest, range)
-{
-    ASSERT_EQ(range<TypeParam>(1, -1), vector<TypeParam>({}));
-    ASSERT_EQ(range<TypeParam>(1, 3), vector<TypeParam>({1, 2}));
-    ASSERT_EQ(range<TypeParam>(1, 4, 2), vector<TypeParam>({1, 3}));
-    ASSERT_EQ(range<TypeParam>(4, 1, 2), vector<TypeParam>({}));
-    ASSERT_EQ(range<TypeParam>(4, 1, -2), vector<TypeParam>({4, 2}));
+/**
+ * Test the arithmetic range() function.
+ */
+TYPED_TEST(ArithmeticRangeTest, no_step) {
+    auto values(range<TypeParam>(1, 3));
+    ASSERT_EQ(vector<TypeParam>(begin(values), end(values)), vector<TypeParam>({1, 2}));
+}
+
+
+/**
+ * Test the arithmetic range() function for an empty range.
+ */
+TYPED_TEST(ArithmeticRangeTest, no_step_empty) {
+    auto values(range<TypeParam>(1, -1));
+    ASSERT_EQ(vector<TypeParam>(begin(values), end(values)), vector<TypeParam>());
+}
+
+
+/**
+ * Test the arithmetic range() function with a step.
+ */
+TYPED_TEST(ArithmeticRangeTest, step) {
+    auto values(range<TypeParam>(1, 4, 2));
+    ASSERT_EQ(vector<TypeParam>(begin(values), end(values)), vector<TypeParam>({1, 3}));
+}
+
+
+/**
+ * Test the arithmetic range() function with a negative step.
+ */
+TYPED_TEST(ArithmeticRangeTest, step_negative) {
+    auto values(range<TypeParam>(4, 1, -2));
+    ASSERT_EQ(vector<TypeParam>(begin(values), end(values)), vector<TypeParam>({4, 2}));
+}
+
+/**
+ * Test the arithmetic range() function for an invalid step.
+ */
+TYPED_TEST(ArithmeticRangeTest, step_zero) {
     ASSERT_THROW(range<TypeParam>(1, 3, 0), std::invalid_argument);
 }
+
